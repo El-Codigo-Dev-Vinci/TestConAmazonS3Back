a4dc5b7ae972a871debd2c361e81cae8c4b89a5d
JoaquinPettinari <JoaquinPettinari@hotmail.com>
2021-03-28 11:51:26 -0300

    Adding save file

--- models/File.js
+++ models/File.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const FileSchema = new mongoose.Schema({
-    content: String,
-    author: String            
+    key: String,
+    nameFile: String,
+    linkFile: String    
 })
 
 module.exports = mongoose.model('File', FileSchema);
--- routes/Files.js
+++ routes/Files.js
@@ -11,10 +11,21 @@ const { uploadFile } = require('../s3')
  })
 
  router.post('/', upload.single('file'), async (req, res) => {    
-   const file = req.file
-   console.log(file)
+   const file = req.file   
    const result = await uploadFile(file)
+   const newFile = new File({
+      key:result.Key,
+      fileName: file.originalname,
+      linkFile: result.Location
+   })
+   console.log(newFile)
    console.log(result)
+   try{
+      const savedFile = await newFile.save();
+      res.json(savedFile);
+   }catch(err){
+      res.json({message: err});
+   }
     //const newFile = new File(file)
     res.send("was fine")
  })
